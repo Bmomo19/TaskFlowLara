1c8b8997c54ecd632fb2f99667b5f784095ba701
Mohammed Moudachir BOURAIMA <mbouraima@fidra.ci>
2025-03-28 16:51:51 +0000

    Update login not working

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\BadResponseException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use App\Models\User;
@@ -37,16 +39,44 @@ class AuthController extends Controller
 
     public function login(Request $request): JsonResponse
     {
-        $credentials = $request->only('email', 'password');
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
 
-        if (!Auth::attempt($credentials)) {
-            return response()->json(['error' => 'Unauthorized'], 401);
+        $http = new Client();
+
+        try {
+//            $response = $http->post(config('app.url') . '/oauth/token', [
+            $response = $http->post('http://127.0.0.1:8000/oauth/token', [
+                'form_params' => [
+                    'grant_type' => 'password',
+                    'client_id' => env('PASSWORD_CLIENT_ID'),
+                    'client_secret' => env('PASSWORD_CLIENT_SECRET'),
+                    'username' => $request->email,
+                    'password' => $request->password,
+                    'scope' => '',
+                ],
+            ]);
+
+            return json_decode((string) $response->getBody(), true);
+        } catch (BadResponseException $e) {
+            return response()->json(['error' => 'Unauthorized', 'error_description' => $e->getMessage()], 401);
         }
-
-        $user = Auth::user();
-        $token = $user->createToken('API Token')->accessToken;
-
-        return response()->json(['user' => $user, 'token' => $token]);
+//        try {
+//            $credentials = $request->only('email', 'password');
+//
+//            if (!Auth::attempt($credentials)) {
+//                return response()->json(['error' => 'Unauthorized'], 401);
+//            }
+//
+//            $user = Auth::user();
+//            $token = $user->createToken('API Token')->accessToken;
+//
+//            return response()->json(['user' => $user, 'token' => $token]);
+//        } catch (\Throwable $e) {
+//            return response()->json(['error' => $e->getMessage()], 500);
+//        }
     }
 
     public function logout(Request $request): JsonResponse
